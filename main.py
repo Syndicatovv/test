47b4eaf4dd66264bcd5903ff49d15df9de668633
Syndicatovv <fgtyttdsss@gmail.com>
2026-04-09 22:27:03 +0300

    change file

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print("Hello World")
     print("develop")
-    print("hi")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
